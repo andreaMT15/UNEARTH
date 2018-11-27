444f851d4827a97a625c96a47d66e0f38f14cce3
Nate <33041144+nathanw88@users.noreply.github.com>
2018-11-26 22:38:34 -0600

    added dropdown menu will make event listener for it tomorrow

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -68,14 +68,58 @@ function fetchTrails(lat, long) {
           response.trails[i].difficulty
         }`
       );
+      var cardDropdown = $("<div class='dropdown'>");
+
+      var dropdownButton = $(
+        '<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'
+      );
+      dropdownButton.text("Search nearby!");
+
+      var dropdownMenu = $(
+        '<div class="dropdown-menu" aria-labelledby="dropdownMenu2">'
+      );
+      var busButton = $(
+        `<button class="dropdown-item" type="button" id="bus" data-lat=${
+          response.trails[i].latitude
+        } data-lng=${response.trails[i].longitude}>`
+      );
+
+      busButton.text("Bus");
+      var trainButton = $(
+        `<button class="dropdown-item" type="button" id="train" data-lat=${
+          response.trails[i].latitude
+        } data-lng=${response.trails[i].longitude}>`
+      );
+      trainButton.text("Train");
+      var airportButton = $(
+        `<button class="dropdown-item" type="button" id="airport" data-lat=${
+          response.trails[i].latitude
+        } data-lng=${response.trails[i].longitude}>`
+      );
+      airportButton.text("Airport");
+      var restaurantButton = $(
+        `<button class="dropdown-item" type="button" id="restaurant" data-lat=${
+          response.trails[i].latitude
+        } data-lng=${response.trails[i].longitude}>`
+      );
+      restaurantButton.text("Restaurant");
+      dropdownMenu.append(busButton);
+      dropdownMenu.append(trainButton);
+      dropdownMenu.append(airportButton);
+      dropdownMenu.append(restaurantButton);
+
+      cardDropdown.append(dropdownButton);
+      cardDropdown.append(dropdownMenu);
       newCard.append(cardImg);
       newCard.append(cardBody);
       newCard.append(cardTitle);
       newCard.append(cardText);
+      newCard.append(cardDropdown);
       $("#trails").append(newCard);
     }
   });
 }
+
 function googlePlace() {
   //required parameter
   var googleLatitude = "41.895481";
