fd3f915f1c515074267feb7079d005bf1fef5688
Nate <33041144+nathanw88@users.noreply.github.com>
2018-11-26 21:16:17 -0600

    cleaned up code

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -74,15 +74,6 @@ function fetchTrails(lat, long) {
       newCard.append(cardText);
       $("#trails").append(newCard);
     }
-
-    //   <div class="card" "col-md-4">
-    //   <img class="card-img-top" src=".../100px180/" alt="Card image cap">
-    //   <div class="card-body">
-    //     <h5 class="card-title">Card title</h5>
-    //     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    //     <a href="#" class="btn btn-primary">Go somewhere</a>
-    //   </div>
-    // </div>
   });
 }
 function googlePlace() {
